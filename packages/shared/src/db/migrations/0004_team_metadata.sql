0d2ef1f805164d2999cd536eb048687c6abea74a
Dan Jennings <dsj1984@gmail.com>
2026-05-22 16:45:32 -0400

    chore(migrations): drop pre-merge 0004_team_metadata; will regen as 0005 after merging epic/10

--- packages/shared/src/db/migrations/0004_team_metadata.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `teams` ADD `sport` text DEFAULT '' NOT NULL;--> statement-breakpoint
-ALTER TABLE `teams` ADD `season` text DEFAULT '' NOT NULL;--> statement-breakpoint
-ALTER TABLE `teams` ADD `age_group` text DEFAULT '' NOT NULL;--> statement-breakpoint
-ALTER TABLE `teams` ADD `archived_at` integer;
